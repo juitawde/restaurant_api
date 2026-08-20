f2115b3209554968b2c78cfa5546305fbeebc6e3
Jui23 <juitawde23@gmail.com>
2026-08-20 16:39:29 +0530

    modified

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000";
+const API_URL = "https://restaurant-api-kappa.vercel.app/";
 
 const request = async (endpoint, options = {}) => {
     const token = localStorage.getItem("token");
