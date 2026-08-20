e81657daaf1c4bfef7c2e9b016d25fdb92f6637b
Jui23 <juitawde23@gmail.com>
2026-08-20 16:47:25 +0530

    added

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://restaurant-api-nviq.onrender.com/";
+const API_URL = "https://restaurant-api-kappa.vercel.app";
 
 const request = async (endpoint, options = {}) => {
     const token = localStorage.getItem("token");
