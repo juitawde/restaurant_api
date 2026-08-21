3d2ee0c1e6dcf8761e2cb1381dfd00a8cba99854
Jui23 <juitawde23@gmail.com>
2026-08-21 16:22:48 +0530

    added

--- JUI_TAWDE_SAM_ALTMAN/server.js
+++ JUI_TAWDE_SAM_ALTMAN/server.js
@@ -24,7 +24,7 @@ app.use(cors({
     origin: [
         "http://localhost:5173",
         "http://localhost:5174",
-        "https://restaurant-api-qwy7.vercel.app"
+        "https://restaurant-api-bq5c.vercel.app"
     ],
     credentials: true
 }));
