7e35a66777f9d2216a42340357e0fb63e806274c
Jui23 <juitawde23@gmail.com>
2026-08-21 15:09:24 +0530

    added

--- JUI_TAWDE_SAM_ALTMAN/server.js
+++ JUI_TAWDE_SAM_ALTMAN/server.js
@@ -22,6 +22,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
     origin: [
+        "http://localhost:5173",
         "http://localhost:5174",
         "https://restaurant-api-qwy7.vercel.app"
     ],
